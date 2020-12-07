2cb1ea229ecca0e197166e5ccc1720e2df440434
IsaLjungdahl <isa.ljungdahl@gmail.com>
2020-12-07 22:26:16 +0100

    test added:returnSumofTwoNumbers,refactored methods, all tests are working

--- src/main/java/stringCalculator/StringCalculator.java
+++ src/main/java/stringCalculator/StringCalculator.java
@@ -6,8 +6,13 @@ public class StringCalculator {
     }
 
     public int Add(String numbers) {
-        if(numbers.isEmpty()){
-        return 0;}
-        return Integer.parseInt(numbers);
+        if (numbers.isEmpty()) {
+            return 0;
+        } else if(numbers.length()<2){
+            return Integer.parseInt(numbers);
+        }
+        String[]numberArr =numbers.split(",");
+        int sum= Integer.parseInt(numberArr[0])+Integer.parseInt(numberArr[1]);
+        return sum;
     }
 }
